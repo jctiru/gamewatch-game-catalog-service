1adbc1a01bed3db5c3744e16feef054270d38bcc
jctiru <jonathanchristiantiru@gmail.com>
2020-06-16 21:49:51 +0800

    Initial commit

---
+++ gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/GamewatchGameCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jctiru.gamewatch.api.gamewatchgamecatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamewatchGameCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamewatchGameCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ gamewatch-game-catalog-service/src/test/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/GamewatchGameCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jctiru.gamewatch.api.gamewatchgamecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamewatchGameCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
