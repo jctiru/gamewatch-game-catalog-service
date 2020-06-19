fafe6c5c1150ca6ca2fd39ffe4190b0b6ae66559
jctiru <jonathanchristiantiru@gmail.com>
2020-06-19 23:24:04 +0800

    Enable feign clients

--- gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/GamewatchGameCatalogServiceApplication.java
+++ gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/GamewatchGameCatalogServiceApplication.java
@@ -2,7 +2,9 @@ package com.jctiru.gamewatch.api.gamewatchgamecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class GamewatchGameCatalogServiceApplication {
 
