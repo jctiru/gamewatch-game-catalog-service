1e291e5b7de869380f5c493b1b656132ce864d5d
jctiru <jonathanchristiantiru@gmail.com>
2020-06-19 23:30:56 +0800

    Create factory class for preconfigured igdb params

---
+++ gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/io/external/model/request/IgdbGameCatalogParamsStringFactory.java
@@ -0,0 +1,38 @@
+package com.jctiru.gamewatch.api.gamewatchgamecatalogservice.io.external.model.request;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class IgdbGameCatalogParamsStringFactory {
+
+	public String getPopularGamesNowIgdbParamsString() {
+		IgdbGameCatalogParams params = new IgdbGameCatalogParams();
+
+		List<String> fields = new ArrayList<>();
+		fields.add("name");
+		fields.add("cover.url");
+		fields.add("cover.url");
+		fields.add("first_release_date");
+		fields.add("genres.name");
+		fields.add("slug");
+
+		List<String> conditions = new ArrayList<>();
+		conditions.add(
+				"first_release_date > " + Instant.now().atZone(ZoneId.systemDefault()).minusMonths(6).toEpochSecond());
+		conditions.add("first_release_date < " + Long.toString(Instant.now().getEpochSecond()));
+		conditions.add("pulse_count > 1");
+
+		params.setFields(fields);
+		params.setConditions(conditions);
+		params.setSort("pulse_count desc");
+		params.setLimit(20);
+
+		return params.getParamsString();
+	}
+
+}
