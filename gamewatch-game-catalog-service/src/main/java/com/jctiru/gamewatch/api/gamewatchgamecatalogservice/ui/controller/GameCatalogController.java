3c36b1b8711c92d5d1e46303e41ac539fcd34413
jctiru <jonathanchristiantiru@gmail.com>
2020-06-19 23:32:00 +0800

    Create GameCatalogController

---
+++ gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/ui/controller/GameCatalogController.java
@@ -0,0 +1,25 @@
+package com.jctiru.gamewatch.api.gamewatchgamecatalogservice.ui.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jctiru.gamewatch.api.gamewatchgamecatalogservice.io.external.model.response.GameOverview;
+import com.jctiru.gamewatch.api.gamewatchgamecatalogservice.service.GameCatalogService;
+
+@RestController
+@RequestMapping("/game-catalog")
+public class GameCatalogController {
+
+	@Autowired
+	private GameCatalogService gameCatalogService;
+
+	@GetMapping("/popular-now")
+	public List<GameOverview> getPopularGamesNow() {
+		return gameCatalogService.getPopularGamesNow();
+	}
+
+}
