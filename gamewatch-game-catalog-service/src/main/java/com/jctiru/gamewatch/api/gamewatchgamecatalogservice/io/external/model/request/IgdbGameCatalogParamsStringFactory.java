459ff24b9b43ebd103196873a66b2ae49068b7bc
jctiru <jonathanchristiantiru@gmail.com>
2020-06-20 23:07:21 +0800

    Create igdb params string factory method for recently released games

--- gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/io/external/model/request/IgdbGameCatalogParamsStringFactory.java
+++ gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/io/external/model/request/IgdbGameCatalogParamsStringFactory.java
@@ -58,4 +58,28 @@ public class IgdbGameCatalogParamsStringFactory {
 		return params.getParamsString();
 	}
 
+	public String getRecentlyReleasedIgdbParamsString() {
+		IgdbGameCatalogParams params = new IgdbGameCatalogParams();
+
+		List<String> fields = new ArrayList<>();
+		fields.add("name");
+		fields.add("cover.url");
+		fields.add("cover.url");
+		fields.add("first_release_date");
+		fields.add("genres.name");
+		fields.add("slug");
+
+		List<String> conditions = new ArrayList<>();
+		conditions.add(
+				"first_release_date > " + Instant.now().atZone(ZoneId.systemDefault()).minusMonths(1).toEpochSecond());
+		conditions.add("first_release_date < " + Long.toString(Instant.now().getEpochSecond()));
+
+		params.setFields(fields);
+		params.setConditions(conditions);
+		params.setSort("first_release_date desc");
+		params.setLimit(20);
+
+		return params.getParamsString();
+	}
+
 }
