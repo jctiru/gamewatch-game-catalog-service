4d4c670880c1fb0e8e31f12c7dceeb584d8c9231
jctiru <jonathanchristiantiru@gmail.com>
2020-06-21 20:59:52 +0800

    Move generation of igdb game overview field params to static method

--- gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/io/external/model/request/IgdbGameCatalogParamsStringFactory.java
+++ gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/io/external/model/request/IgdbGameCatalogParamsStringFactory.java
@@ -13,21 +13,13 @@ public class IgdbGameCatalogParamsStringFactory {
 	public String getPopularGamesNowIgdbParamsString() {
 		IgdbGameCatalogParams params = new IgdbGameCatalogParams();
 
-		List<String> fields = new ArrayList<>();
-		fields.add("name");
-		fields.add("cover.url");
-		fields.add("cover.url");
-		fields.add("first_release_date");
-		fields.add("genres.name");
-		fields.add("slug");
-
 		List<String> conditions = new ArrayList<>();
 		conditions.add(
 				"first_release_date > " + Instant.now().atZone(ZoneId.systemDefault()).minusMonths(6).toEpochSecond());
 		conditions.add("first_release_date < " + Long.toString(Instant.now().getEpochSecond()));
 		conditions.add("pulse_count > 1");
 
-		params.setFields(fields);
+		params.setFields(IgdbGameCatalogParamsStringFactory.getGameOverviewFieldParams());
 		params.setConditions(conditions);
 		params.setSort("pulse_count desc");
 		params.setLimit(20);
@@ -38,19 +30,11 @@ public class IgdbGameCatalogParamsStringFactory {
 	public String getMostAnticipatedIgdbParamsString() {
 		IgdbGameCatalogParams params = new IgdbGameCatalogParams();
 
-		List<String> fields = new ArrayList<>();
-		fields.add("name");
-		fields.add("cover.url");
-		fields.add("cover.url");
-		fields.add("first_release_date");
-		fields.add("genres.name");
-		fields.add("slug");
-
 		List<String> conditions = new ArrayList<>();
 		conditions.add("first_release_date > " + Long.toString(Instant.now().getEpochSecond()));
 		conditions.add("pulse_count > 1");
 
-		params.setFields(fields);
+		params.setFields(IgdbGameCatalogParamsStringFactory.getGameOverviewFieldParams());
 		params.setConditions(conditions);
 		params.setSort("pulse_count desc");
 		params.setLimit(20);
@@ -61,20 +45,12 @@ public class IgdbGameCatalogParamsStringFactory {
 	public String getRecentlyReleasedIgdbParamsString() {
 		IgdbGameCatalogParams params = new IgdbGameCatalogParams();
 
-		List<String> fields = new ArrayList<>();
-		fields.add("name");
-		fields.add("cover.url");
-		fields.add("cover.url");
-		fields.add("first_release_date");
-		fields.add("genres.name");
-		fields.add("slug");
-
 		List<String> conditions = new ArrayList<>();
 		conditions.add(
 				"first_release_date > " + Instant.now().atZone(ZoneId.systemDefault()).minusMonths(1).toEpochSecond());
 		conditions.add("first_release_date < " + Long.toString(Instant.now().getEpochSecond()));
 
-		params.setFields(fields);
+		params.setFields(IgdbGameCatalogParamsStringFactory.getGameOverviewFieldParams());
 		params.setConditions(conditions);
 		params.setSort("first_release_date desc");
 		params.setLimit(20);
@@ -85,24 +61,26 @@ public class IgdbGameCatalogParamsStringFactory {
 	public String getComingSoonIgdbParamsString() {
 		IgdbGameCatalogParams params = new IgdbGameCatalogParams();
 
-		List<String> fields = new ArrayList<>();
-		fields.add("name");
-		fields.add("cover.url");
-		fields.add("cover.url");
-		fields.add("first_release_date");
-		fields.add("genres.name");
-		fields.add("slug");
-
 		List<String> conditions = new ArrayList<>();
 		conditions.add("first_release_date > " + Long.toString(Instant.now().getEpochSecond()));
 
-		params.setFields(fields);
+		params.setFields(IgdbGameCatalogParamsStringFactory.getGameOverviewFieldParams());
 		params.setConditions(conditions);
 		params.setSort("first_release_date asc");
 		params.setLimit(20);
 
 		return params.getParamsString();
+	}
 
+	private static List<String> getGameOverviewFieldParams() {
+		List<String> fields = new ArrayList<>();
+		fields.add("name");
+		fields.add("cover.url");
+		fields.add("first_release_date");
+		fields.add("genres.name");
+		fields.add("slug");
+
+		return fields;
 	}
 
 }
