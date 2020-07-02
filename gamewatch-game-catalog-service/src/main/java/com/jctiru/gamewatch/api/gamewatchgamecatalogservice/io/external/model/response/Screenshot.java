d0e00caa3e68a45c3037a79f2b5532dd00a26bd1
jctiru <jonathanchristiantiru@gmail.com>
2020-07-02 23:56:06 +0800

    Create screenshot model

---
+++ gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/io/external/model/response/Screenshot.java
@@ -0,0 +1,33 @@
+package com.jctiru.gamewatch.api.gamewatchgamecatalogservice.io.external.model.response;
+
+public class Screenshot {
+
+	private long id;
+	private String image_id;
+	private String url;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getImage_id() {
+		return image_id;
+	}
+
+	public void setImage_id(String image_id) {
+		this.image_id = image_id;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
