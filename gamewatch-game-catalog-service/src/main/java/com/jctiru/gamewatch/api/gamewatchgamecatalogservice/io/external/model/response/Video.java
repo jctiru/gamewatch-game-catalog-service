7a0db43fa1a81fd82a9315926c9c82dc1f05e426
jctiru <jonathanchristiantiru@gmail.com>
2020-07-03 00:26:31 +0800

    Create video model

---
+++ gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/io/external/model/response/Video.java
@@ -0,0 +1,33 @@
+package com.jctiru.gamewatch.api.gamewatchgamecatalogservice.io.external.model.response;
+
+public class Video {
+
+	private long id;
+	private String name;
+	private String video_id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getVideo_id() {
+		return video_id;
+	}
+
+	public void setVideo_id(String video_id) {
+		this.video_id = video_id;
+	}
+
+}
