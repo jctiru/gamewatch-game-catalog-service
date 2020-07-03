2dce6e14c471c5da535faf8b255cf358b28ba4b6
jctiru <jonathanchristiantiru@gmail.com>
2020-07-03 23:16:10 +0800

    Create company model

---
+++ gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/io/external/model/response/Company.java
@@ -0,0 +1,24 @@
+package com.jctiru.gamewatch.api.gamewatchgamecatalogservice.io.external.model.response;
+
+public class Company {
+
+	private long id;
+	private String name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
