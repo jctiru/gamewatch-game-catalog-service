c856877473a620053cfda8b97d6c9d47a3ae85c6
jctiru <jonathanchristiantiru@gmail.com>
2020-07-04 02:16:48 +0800

    Add getGameDetails in igdb game catalog feign client

--- gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/io/external/IgdbGameCatalogServiceClient.java
+++ gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/io/external/IgdbGameCatalogServiceClient.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.jctiru.gamewatch.api.gamewatchgamecatalogservice.io.external.model.response.GameDetails;
 import com.jctiru.gamewatch.api.gamewatchgamecatalogservice.io.external.model.response.GameOverview;
 
 @FeignClient(name = "igdb-game-catalog-service", url = "https://api-v3.igdb.com/games")
@@ -13,5 +14,8 @@ public interface IgdbGameCatalogServiceClient {
 
 	@PostMapping
 	public List<GameOverview> getGamesList(@RequestBody String igdbParams);
+
+	@PostMapping
+	public List<GameDetails> getGameDetails(@RequestBody String igdbParams);
 
 }
