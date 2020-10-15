6ae57d44dece20714ca88d7e0a6330973cbe9c3b
jctiru <jonathanchristiantiru@gmail.com>
2020-10-15 22:05:29 +1000

    Remove global feign client config

--- gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/io/external/IgdbGameCatalogServiceClientConfiguration.java
+++ gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/io/external/IgdbGameCatalogServiceClientConfiguration.java
@@ -1,24 +1,15 @@
 package com.jctiru.gamewatch.api.gamewatchgamecatalogservice.io.external;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-
-import com.jctiru.gamewatch.api.gamewatchgamecatalogservice.AppPropertiesFile;
 
 import feign.Logger;
 import feign.RequestInterceptor;
 
-@Configuration
 public class IgdbGameCatalogServiceClientConfiguration {
 
 	@Bean
-	public RequestInterceptor requestInterceptor(AppPropertiesFile appPropertiesFile) {
-
-		return requestTemplate -> {
-			requestTemplate.header("Accept", MediaType.APPLICATION_JSON_VALUE);
-			requestTemplate.header("user-key", appPropertiesFile.getIgdbApiKey());
-		};
+	public RequestInterceptor requestInterceptor() {
+		return new IgdbFeignClientRequestInterceptor();
 	}
 
 	@Bean
