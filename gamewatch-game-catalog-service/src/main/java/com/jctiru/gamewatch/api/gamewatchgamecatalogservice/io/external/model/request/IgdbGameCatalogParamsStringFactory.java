256b2bf1e06562484187b5198b26964054da39b6
Jonathan Christian S. Tiru <jonathanchristiantiru@gmail.com>
2025-02-04 02:01:50 -0800

    Update IgdbGameCatalogParamsStringFactory.java temp fix for deprecated api follow

--- gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/io/external/model/request/IgdbGameCatalogParamsStringFactory.java
+++ gamewatch-game-catalog-service/src/main/java/com/jctiru/gamewatch/api/gamewatchgamecatalogservice/io/external/model/request/IgdbGameCatalogParamsStringFactory.java
@@ -17,11 +17,13 @@ public class IgdbGameCatalogParamsStringFactory {
 		conditions.add(
 				"first_release_date > " + Instant.now().atZone(ZoneId.systemDefault()).minusMonths(6).toEpochSecond());
 		conditions.add("first_release_date < " + Long.toString(Instant.now().getEpochSecond()));
-		conditions.add("follows != null");
+		// conditions.add("follows != null");
+		conditions.add("hypes != null");
 
 		params.setFields(IgdbGameCatalogParamsStringFactory.getGameOverviewFieldParams());
 		params.setConditions(conditions);
-		params.setSort("follows desc");
+		// params.setSort("follows desc");
+		params.setSort("hypes desc");
 		params.setLimit(20);
 
 		return params.getParamsString();
